9733d4abbbb5529e853460e0f50473969efcc1f3
tristan-genspark <tristan.stringfellow@genspark.net>
2022-07-18 08:45:20 -0500

    Changing Directorys

--- lab week 3 day 1 - python and java/test.bat
+++
@@ -1,4 +0,0 @@
-
-python main.py
-
-pause
--- lab week 3 day 5 - create ci cd pipeline with jenkins and terraform/Step 1/manual-build-docker.bat
+++
@@ -1,8 +0,0 @@
-@echo OFF
-cd "Build-Server"
-docker build --no-cache -t http-py-server .
-echo.
-echo To run the container continue this script
-pause
-docker run -d -p 8081:80 http-py-server
-pause
